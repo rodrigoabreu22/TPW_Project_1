5b498e336871f6e241a6eb6ff005a4522a16b64e
Ricardo <ricardo.alexandre.antunes@ua.pt>
2024-10-24 00:27:22 +0100

    ok agora a serio

--- AmorCamisola/models.py
+++ AmorCamisola/models.py
@@ -47,6 +47,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     team = models.CharField(max_length=50, null=True)
     description = models.TextField()
+    sold = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
