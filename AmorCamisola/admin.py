26a02ae9b80f51e57266ebd6ecd69b234e176644
Ricardo <ricardo.alexandre.antunes@ua.pt>
2024-10-23 23:50:36 +0100

    admin stuff

--- AmorCamisola/admin.py
+++ AmorCamisola/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from AmorCamisola.models import *
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Following)
+admin.site.register(Moderador)
+admin.site.register(Produto)
+admin.site.register(Favorite)
+admin.site.register(Camisola)
+admin.site.register(Calcoes)
+admin.site.register(Meias)
+admin.site.register(Chuteiras)
