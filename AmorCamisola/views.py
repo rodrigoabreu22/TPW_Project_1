36ab766734d24804d9666d12a3a13e99626194cf
rodrigoabreu22 <rgana22svb@gmail.com>
2024-10-23 22:21:19 +0100

    penso que se pode testar a adição de um utilizador

--- AmorCamisola/views.py
+++ AmorCamisola/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from datetime import datetime
+from .models import *
+from django.http import HttpResponse
+from .forms import *
+
+from django.contrib.auth import authenticate, login as auth_login
+from django.shortcuts import render, redirect
+
+from django.shortcuts import get_object_or_404
+from django.views.decorators.http import require_POST
+from django.http import HttpResponse
+from .models import *
+from django.db.models import Q
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
+
+def home(request):
+    tparams = {
+        'title': 'Home Page',
+        'year': datetime.now().year,
+    }
+    return render(request, 'index.html', tparams)
+
+
+
+def register(request):
+    if request.method == 'POST':
+        form = CreateAccountForm(request.POST)
+        print(form.errors)
+
+        if form.is_valid():
+            # Check if user already exists
+            u = User.objects.filter(username=form.cleaned_data['username'])
+
+            if u:
+                return render(request, 'register.html', {'form': form, 'error': True})
+
+            else:
+                form.save()
+                email = form.cleaned_data.get('email')
+                username = form.cleaned_data.get('username')
+                raw_password = form.cleaned_data.get('password1')
+
+                # Authenticate user then login
+                user = authenticate(username=username, password=raw_password)
+                auth_login(request, user)
+
+                # Create user in our database
+                user = User.objects.create(username=username, email=email, password=raw_password,
+                                           name=form.cleaned_data['name'])
+                user.save()
+
+                return redirect('/')
+        else:
+            return render(request, 'register.html', {'form': form, 'error': True})
+    else:
+        form = RegisterForm()
+        return render(request, 'register.html', {'form': form, 'error': False})
--- TPW_Project_1/urls.py
+++ TPW_Project_1/urls.py
@@ -1,22 +1,50 @@
-"""
-URL configuration for TPW_Project_1 project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from AmorCamisola import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', views.home, name='home'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+
+    path('books/', views.books, name='books'),
+
+    path('books/<int:id>/', views.bookDetails, name='bookDetails'),
+
+    path('authors/', views.authors, name='authors'),
+
+    path('authors/<int:id>/', views.authorDetails, name='authorDetails'),
+
+    path('publishers/', views.publishers, name='publishers'),
+
+    path('publishers/<int:id>/', views.publisherDetails, name='publisherDetails'),
+
+    path('author/<int:author_id>/books', views.booksByAuthor, name='booksByAuthor'),
+
+    path('publishers/<int:pub_id>/authors', views.publisherAuthors, name='publisherAuthors'),
+
+    path('booksearch/', views.booksearch, name='booksearch'),
+
+    path('authorsearch/', views.searchAuthor2, name='authorsearch'),
+
+    path('booksearchAP/', views.booksearchAP2, name='booksearchAP'),
+
+    path('insertauthor/', views.insertAuthor2, name='insertAuthor'),
+
+    path('insertpublisher/', views.insertPublisher2, name='insertPublisher'),
+
+    path('insertbook/', views.insertBook2, name='insertBook'),
+
+    path('bookquery/', views.bookquery, name='bookquery'),
+
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+
+    path('logout', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+
+    path('cart', views.purchased_books, name='cart'),
+
+    path('buy_book/<int:id>', views.buy_book, name='buy_book'),
 ]
