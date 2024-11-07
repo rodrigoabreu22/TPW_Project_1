120f691be689b52d44e9cbd20ad0685cab2eb0d9
rodrigoabreu22 <rgana22svb@gmail.com>
2024-11-07 16:06:54 +0000

    pequeno fix

--- TPW_Project_1/urls.py
+++ TPW_Project_1/urls.py
@@ -13,8 +13,8 @@ urlpatterns = [
 
     path('profile/<str:username>', views.viewProfile, name='profile'),
     path('publishproduct/', views.pubProduct, name='publishproduct'),
-    path('login/',auth_views.LoginView.as_view(template_name="login.html", next_page="myprofile"), name='login'),
-    path('logout', auth_views.LogoutView.as_view(next_page='publishproduct'), name='logout'),
+    path('login/',auth_views.LoginView.as_view(template_name="login.html", next_page="home"), name='login'),
+    path('logout', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
     path('',views.home, name='home'),
     path('follow/<str:username>/', views.follow_user, name='follow'),
     path('unfollow/<str:username>/', views.unfollow_user, name='unfollow'),
